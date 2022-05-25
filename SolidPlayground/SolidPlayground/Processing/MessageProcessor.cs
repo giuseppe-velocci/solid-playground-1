3b6e7042f032a008923874f48fb1895303448fc5
Giuseppe Velocci <runnermean@gmail.com>
2022-05-25 20:55:27 +0200

    notes

---
+++ SolidPlayground/SolidPlayground/Processing/MessageProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPlayground.Processing
+{
+    public class MessageProcessor
+    {
+
+        #region Handle EquipmentActivity
+        //
+        //  If message Body contains "ActivityId":
+        //      Check if BookingNumber Exists on Storage
+        //      Y: Publish EquipmentActivity
+        //      N: Store EquipmentActivity
+        //
+        #endregion
+
+
+        #region Handle Booking
+        //
+        //  If message Body contains "BookingNumber":
+        //      Check if booking Exists on Storage:
+        //  Y: Ignore
+        //  N: Store BookingNumber
+        //
+        #endregion
+    }
+}
--- SolidPlayground/SolidPlayground/Program.cs
+++ SolidPlayground/SolidPlayground/Program.cs
@@ -2,9 +2,7 @@
 var logger = loggerFactory.CreateLogger<Program>();
 logger.LogInformation("Starting message processor");
 
-
-
-
+#region Digital factory Platform overview
 //                                   ________________________
 //      (((((((((((((((((((() --->  |                        |
 //     Booking messages queue       |                        |
@@ -15,5 +13,29 @@ logger.LogInformation("Starting message processor");
 //    Equipment messages queue      |________________________|
 //                                        Digital Factory
 //                                           Platform
+#endregion
+
+#region Create Booking Subscriber
+//
+// 1. create a Subscriber for Booking queue
+//                                   ________________
+//      (((((((((((((((((((() --->  |                |
+//     Booking messages queue       | handle Booking |
+//                                  |________________|
+//                                  Messages Processor
+//
+#endregion
+
+
+#region Create EquipmentActivities Subscriber
+//
+// 2. create a Subscriber for EquipmentActivites queue
+//                                   __________________
+//      (((((((((((((((((((() --->  |                  |
+//     Equipment messages queue     | handle Equipment |
+//                                  |__________________|
+//                                   Messages Processor
+//
+#endregion
 
 Console.ReadLine();
