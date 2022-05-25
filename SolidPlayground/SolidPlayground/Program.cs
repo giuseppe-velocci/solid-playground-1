61e1147b7067ebf38bca51a53814544e22444bf4
Giuseppe Velocci <runnermean@gmail.com>
2022-05-25 06:43:19 +0200

    intro graphic in program

--- SolidPlayground/SolidPlayground/Program.cs
+++ SolidPlayground/SolidPlayground/Program.cs
@@ -1,7 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Core.DTO;
+using MessagesFramework;
 using Microsoft.Extensions.Logging;
 
 var loggerFactory = new Infrastructure.Logging.LogServiceFactory();
 var logger = loggerFactory.CreateLogger<Program>();
 logger.LogInformation("Starting message processor");
 Console.ReadLine();
+
+
+//                               ___________________________
+//   (((((((((((((((((((() ---> |                           |
+//  Booking messages queue      |                           |
+//                              |                           |
+//                              |                           | ---> (((((((((((((((((((((((((()
+//                              |                           |          Equipment messages 
+//   (((((((((((((((((((() ---> |                           |        in DCSA standard format
+//  Equipment messages queue    |___________________________|
+//                                     Digital Factory
+//                                        Platform
