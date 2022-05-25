8fa0a98ea52bd41d9215c526ef0f8474790da561
Giuseppe Velocci <runnermean@gmail.com>
2022-05-25 06:56:32 +0200

    completed global using

--- SolidPlayground/SolidPlayground/Program.cs
+++ SolidPlayground/SolidPlayground/Program.cs
@@ -1,21 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Core.DTO;
-using MessagesFramework;
-using Microsoft.Extensions.Logging;
-
-var loggerFactory = new Infrastructure.Logging.LogServiceFactory();
+﻿var loggerFactory = new LogServiceFactory();
 var logger = loggerFactory.CreateLogger<Program>();
 logger.LogInformation("Starting message processor");
+
+
+
+
+//                                   ________________________
+//      (((((((((((((((((((() --->  |                        |
+//      Booking messages queue      |                        |
+//                                  |                        |
+//                                  |                        | ---> (((((((((((((((((((((((((()
+//                                  |                        |          Equipment messages 
+//      (((((((((((((((((((() --->  |                        |        in DCSA standard format
+//    Equipment messages queue      |________________________|
+//                                        Digital Factory
+//                                           Platform
+
 Console.ReadLine();
-
-
-//                               ___________________________
-//   (((((((((((((((((((() ---> |                           |
-//  Booking messages queue      |                           |
-//                              |                           |
-//                              |                           | ---> (((((((((((((((((((((((((()
-//                              |                           |          Equipment messages 
-//   (((((((((((((((((((() ---> |                           |        in DCSA standard format
-//  Equipment messages queue    |___________________________|
-//                                     Digital Factory
-//                                        Platform
